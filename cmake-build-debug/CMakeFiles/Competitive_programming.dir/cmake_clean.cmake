3ce0e669c99f80a99958b2babb2a94bdf5b5ee87
Dubinin Dmitrii <dmitriid204@ya.ru>
2021-12-24 16:04:39 +0500

    Install Clion

---
+++ cmake-build-debug/CMakeFiles/Competitive_programming.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Competitive_programming.dir/1.cpp.obj"
+  "Competitive_programming.exe"
+  "Competitive_programming.exe.manifest"
+  "Competitive_programming.pdb"
+  "libCompetitive_programming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Competitive_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
